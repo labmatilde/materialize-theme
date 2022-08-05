2a38c7cc5a3cf28203601fdbde8ec9ad18b70b82
Eduardo Dalhke Lopes <dalhkedu@gmail.com>
2022-08-05 13:34:05 -0300

    https link

--- header.php
+++ header.php
@@ -13,7 +13,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
-<link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <?php wp_head(); ?>
--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,9 @@
 'use strict';
 
+/**
 $(document).ready(function () {
 	$('.slider').slider();
 	$('.carousel').carousel();
 	$('.button-collapse').sideNav();
 });	
+*/
