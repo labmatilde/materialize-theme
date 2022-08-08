1866adcaa5648921819d161564b92f4ca618dd93
Eduardo Dalhke Lopes <dalhkedu@gmail.com>
2022-08-08 12:48:52 -0300

    mostrando header

--- functions.php
+++ functions.php
@@ -8,79 +8,79 @@
  */
 require 'materialize-functions/materialize-pagination.php';
 
-if ( ! function_exists( 'materialize_css_setup' ) ) :
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
- */
-function materialize_css_setup() {
-	/*
-	 * Make theme available for translation.
-	 * Translations can be filed in the /languages/ directory.
-	 * If you're building a theme based on materialize css, use a find and replace
-	 * to change 'materialize-css' to the name of your theme in all the template files.
-	 */
-	load_theme_textdomain( 'materialize-css', get_template_directory() . '/languages' );
-
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
-
-	/*
-	 * Let WordPress manage the document title.
-	 * By adding theme support, we declare that this theme does not use a
-	 * hard-coded <title> tag in the document head, and expect WordPress to
-	 * provide it for us.
-	 */
-	add_theme_support( 'title-tag' );
-
-	add_theme_support( 'post-formats', array( 'status', 'gallery', 'image', 'video', 'link' ) );
-
-	add_theme_support( 'custom-logo', array(
-			'height'      => 64,
-			'width'       => 400,
-			'flex-width' => true)
-	);
-
-	/*
-	 * Enable support for Post Thumbnails on posts and pages.
+if ( function_exists( 'materialize_css_setup' ) ) :
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
-	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
 	 */
-	add_theme_support( 'post-thumbnails' );
+	function materialize_css_setup() {
+		/*
+		* Make theme available for translation.
+		* Translations can be filed in the /languages/ directory.
+		* If you're building a theme based on materialize css, use a find and replace
+		* to change 'materialize-css' to the name of your theme in all the template files.
+		*/
+		load_theme_textdomain( 'materialize-css', get_template_directory() . '/languages' );
 
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus(
-		array(
-			'primary' => esc_html('Primary'),
-			'secondary' => esc_html('Secondary')
-		)
-	);
-	/*
-	 * Switch default core markup for search form, comment form, and comments
-	 * to output valid HTML5.
-	 */
-	add_theme_support( 'html5', array(
-		'search-form',
-		'comment-form',
-		'comment-list',
-		'gallery',
-		'caption',
-	) );
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support( 'automatic-feed-links' );
 
-	// Set up the WordPress core custom background feature.
-	add_theme_support( 'custom-background', apply_filters( 'materialize_css_custom_background_args', array(
-		'default-color' => 'ffffff',
-		'default-image' => '',
-	) ) );
+		/*
+		* Let WordPress manage the document title.
+		* By adding theme support, we declare that this theme does not use a
+		* hard-coded <title> tag in the document head, and expect WordPress to
+		* provide it for us.
+		*/
+		add_theme_support( 'title-tag' );
 
-	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
-}
+		add_theme_support( 'post-formats', array( 'status', 'gallery', 'image', 'video', 'link' ) );
+
+		add_theme_support( 'custom-logo', array(
+				'height'      => 64,
+				'width'       => 400,
+				'flex-width' => true)
+		);
+
+		/*
+		* Enable support for Post Thumbnails on posts and pages.
+		*
+		* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		*/
+		add_theme_support( 'post-thumbnails' );
+
+		// This theme uses wp_nav_menu() in one location.
+		register_nav_menus(
+			array(
+				'primary' => esc_html('Primary'),
+				'secondary' => esc_html('Secondary')
+			)
+		);
+		/*
+		* Switch default core markup for search form, comment form, and comments
+		* to output valid HTML5.
+		*/
+		add_theme_support( 'html5', array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+		) );
+
+		// Set up the WordPress core custom background feature.
+		add_theme_support( 'custom-background', apply_filters( 'materialize_css_custom_background_args', array(
+			'default-color' => 'ffffff',
+			'default-image' => '',
+		) ) );
+
+		// Add theme support for selective refresh for widgets.
+		add_theme_support( 'customize-selective-refresh-widgets' );
+	}
+	add_action( 'after_setup_theme', 'materialize_css_setup' );
 endif;
-add_action( 'after_setup_theme', 'materialize_css_setup' );
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -179,45 +179,43 @@ require get_template_directory() . '/inc/customizer.php';
 require get_template_directory() . '/inc/jetpack.php';
 
 /* Colunistas */
-function colunista()
-{
+function colunista() {
     global $wpdb;
     $colunista_list = $wpdb->get_results("SELECT ID, user_nicename from $wpdb->users ORDER BY display_name");
     return $colunista_list;
 }
 
 /*Customizar cores*/
-function materialize_controls($wp_customize)
-{
+function materialize_controls($wp_customize) {
 	$wp_customize->add_section( 'materialize_options',
- 	array(
-   'title'      => __('Material Options','materialized'),
-   'description' => 'Personalize seu tema wordpress:',
-   'priority'   => 30,
- )
- );
+		array(
+			'title'      => __('Material Options','materialized'),
+			'description' => 'Personalize seu tema wordpress:',
+			'priority'   => 30,
+		)
+ 	);
  
 // Add setting
- $wp_customize->add_setting(
-    'materialize_colors',
-    array('default' => 'gnl.css')
- );
+$wp_customize->add_setting(
+	'materialize_colors',
+		array('default' => 'gnl.css')
+);
  
 // Add control 
-	$wp_customize->add_control(
-		'materialize_color_selector',
-		array(
-		    'label' => 'Paleta de Cores',
-		    'section' => 'materialize_options',
-		    'settings' => 'materialize_colors',
-		    'type' => 'select',
-		    'choices' => array(
-		    	'vermelho.css' =>'Vermelho',
-		        'azul.css' => 'Azul',
-		        'preto.css' => 'Preto',
-		        'verde.css' => 'Verde',
-		        'laranja.css' => 'Laranja'),
-		));
+$wp_customize->add_control(
+	'materialize_color_selector',
+	array(
+		'label' => 'Paleta de Cores',
+		'section' => 'materialize_options',
+		'settings' => 'materialize_colors',
+		'type' => 'select',
+		'choices' => array(
+			'vermelho.css' =>'Vermelho',
+			'azul.css' => 'Azul',
+			'preto.css' => 'Preto',
+			'verde.css' => 'Verde',
+			'laranja.css' => 'Laranja'),
+	));
 }
 add_action('customize_register', 'materialize_controls');
 
--- header.php
+++ header.php
@@ -22,42 +22,69 @@
 <body <?php body_class(); ?>>
 <div id="page" class="site">
 
-
 	<header id="masthead" class="site-header" role="banner">
-		    <nav class="nav-extended">
-		        <div class="nav-wrapper">		        	
-				    <a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" class="brand-logo center">
-					    <?php
-			        		$custom_logo_id = get_theme_mod( 'custom_logo' );
-			        		if($custom_logo_id)
-			        		{
-			        			$image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
-								echo '<img src="'.$image[0].'">';
-			        		}
-			        		else
-			        		{
-			        			echo '<p class="blog-title">'.bloginfo('name').'</p>';
-			        		}					
-			        	?>	        	
-				    </a>
 
-					<ul id="nav-mobile" class="right hide-on-med-and-down side-nav">
+	<?php if( have_rows('dados', 'option') ):
+    while( have_rows('dados', 'option') ) : the_row(); ?>
+	<pre>
+		<?php print_r(get_sub_field('mostrar')); ?>
+	</pre>
+
+		<!-- TOP MENU -->
+		<div id="topbar" class="menu-top-bar hide-on-med-and-down">
+			<div class="container">
+				<div class="nav-wrapper">
+					<ul class="topbar">
+						<li class="list-top"><i class="material-icons">account_balance</i><span class="content-top">CHAVE
+								PIX: 40.416.297/0001-95</span></li>
+						<li class="list-top"><i class="material-icons">call</i><a class="link-top" href="#"><span
+									class="content-top">(11) 93719-0613</span></a></li>
+						<li class="list-top"><i class="material-icons">call</i><a class="link-top" href="#"><span
+									class="content-top">(11) 2053-0090</span></a></li>
+						<li class="list-top"><i class="material-icons">email</i><a class="link-top"
+								href="mailto:contato@larassistencialmatilde.com.br"><span
+									class="content-top">contato@larassistencialmatilde.com.br</span></a></li>
+					</ul> <!-- .topbar -->
+				</div> <!-- .nav-wrapper -->
+			</div> <!-- .container -->
+		</div> <!-- #topbar -->
+	
+	<?php endwhile; endif; ?>
+
+		<nav class="nav-extended">
+			<div class="nav-wrapper">		        	
+				<a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" class="brand-logo center">
+					<?php
+						$custom_logo_id = get_theme_mod( 'custom_logo' );
+						if($custom_logo_id)
+						{
+							$image = wp_get_attachment_image_src( $custom_logo_id , 'full' );
+							echo '<img src="'.$image[0].'">';
+						}
+						else
+						{
+							echo '<p class="blog-title">'.bloginfo('name').'</p>';
+						}					
+					?>	        	
+				</a>
+
+				<ul id="nav-mobile" class="right hide-on-med-and-down side-nav">
+					<?php wp_nav_menu( array('theme_location' => 'secondary')); ?>
+				</ul>
+	
+				<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+
+				<div class="side-nav" id="mobile-demo">
+						<h5 class="center mobile-title"><?php bloginfo('name'); ?></h5>
 						<?php wp_nav_menu( array('theme_location' => 'secondary')); ?>
-					</ul>
-		
-			    	<a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
-
-				    <div class="side-nav" id="mobile-demo">
-						    <h5 class="center mobile-title"><?php bloginfo('name'); ?></h5>
-				        	<?php wp_nav_menu( array('theme_location' => 'secondary')); ?>
-				        	<?php wp_nav_menu( array( 'theme_location' => 'primary')); ?>	
-			        </div>
-		    	</div>
-		      <div class="nav-content hide-on-med-and-down">
-		        <ul>
-					<?php wp_nav_menu( array('theme_location' => 'primary')); ?>
-		        </ul>
-		      </div>
-		    </nav>
+						<?php wp_nav_menu( array( 'theme_location' => 'primary')); ?>	
+				</div>
+			</div>
+			<div class="nav-content hide-on-med-and-down">
+			<ul>
+				<?php wp_nav_menu( array('theme_location' => 'primary')); ?>
+			</ul>
+			</div>
+		</nav>
 	</header><!-- #masthead -->
 	<div id="content" class="row">
